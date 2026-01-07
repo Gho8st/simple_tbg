bece7d4676b52c8c232b345667153ec4d8b65643
Jaden Brooklyn Lucas <26195291@sun.ac.za>
2026-01-07 19:52:06 +0200

    Made some changes on battle state

--- include/battle_state.h
+++ include/battle_state.h
@@ -2,12 +2,18 @@
 #define BATTLE_STATE_H
 
 #include "state.h"
+#include "entity.h"
 
 class StateManager;
 
 class BattleState : public State {
     private:
         StateManager& manager;
+        
+        Entity player;
+        Entity enemy;
+        bool ifPlayerTurn = true;
+
     public:
         BattleState(StateManager& manager);
         void update() override;
--- src/states/battle_state.cpp
+++ src/states/battle_state.cpp
@@ -1,16 +1,39 @@
 #include "battle_state.h"
 #include "state_manager.h"
 #include "raylib.h"
+#include "raygui.h"
 
-BattleState::BattleState(StateManager& manager) : manager(manager) {
+BattleState::BattleState(StateManager& manager) : manager(manager),
+    player(20, 100),
+    enemy(15, 100) 
+{
 
 }
 
 
 void BattleState::update() {
-
+    if (!ifPlayerTurn) {
+        player.take_damage(enemy.get_attack());
+        ifPlayerTurn = !ifPlayerTurn;
+    }
 }
 
 void BattleState::draw() {
     DrawText("Welcome to Battle Screen", 200, 200, 20, GRAY);
+    DrawText(TextFormat("Player- HP: %d | ATK: %d", player.get_health(), player.get_attack()), 200, 250, 20, BLACK);
+    DrawText(TextFormat("Enemy- HP: %d | ATK: %d", enemy.get_health(), enemy.get_attack()), 200, 270, 20, BLACK);
+
+    DrawCircle(250, 400, 25, GREEN);
+    DrawCircle(475, 400, 25, RED);
+    
+    if (ifPlayerTurn) {
+        if (GuiButton({200, 300, 100, 25}, "Attack")) {
+            enemy.take_damage(player.get_attack());
+            ifPlayerTurn = !ifPlayerTurn;
+        }
+        if (GuiButton({300, 300, 100, 25}, "Heal")) {
+            player.heal(20);
+            ifPlayerTurn = !ifPlayerTurn;
+        }
+    }
 }
