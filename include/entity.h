870ab7027805f5a25e23b2f068642afff7af67e0
Jaden Brooklyn Lucas <26195291@sun.ac.za>
2026-01-08 22:29:45 +0200

    Added more stats to Entity class

--- include/entity.h
+++ include/entity.h
@@ -1,16 +1,25 @@
 #ifndef ENTITY
 #define ENITTY
 
+#include <string>
+
 class Entity {
     private:
         int health;
         int max_health;
         int attack;
+        int defense;
+
+        std::string id;
         
     public:
-        Entity(int atk, int hp);
+        Entity(std::string id, int hp, int atk, int def);
+
         int get_attack();
         int get_health();
+        int get_max_health();
+        int get_defense();
+        std::string get_id();
 
         void take_damage(int dmg);
         void heal(int hp);
--- src/entity.cpp
+++ src/entity.cpp
@@ -2,9 +2,11 @@
 #include <algorithm>
 using namespace std;
 
-Entity::Entity(int atk, int hp) {
+Entity::Entity(string name, int hp, int atk, int def) {
+    id = name;
     attack = atk;
     max_health = health = hp;
+    defense = def;
 }
 
 void Entity::take_damage(int dmg) {
@@ -17,3 +19,6 @@ void Entity::heal(int hp) {
 
 int Entity::get_attack() {return attack;}
 int Entity::get_health() {return health;}
+int Entity::get_max_health() {return max_health;}
+int Entity::get_defense() {return defense;}
+string Entity::get_id() {return id;}
